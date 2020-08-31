25cb16bb01c0592272b71b14203ea82e7990d716
johan Bautista <jsbparra@gmail.com>
2020-08-31 17:56:10 +0200

    agregar nuevo producto al state

--- src/actions/productoAction.js
+++ src/actions/productoAction.js
@@ -6,7 +6,26 @@ import {
 
 // crear nuevo producto
 export function crearNuevoProductoAction(producto) {
-  return () => {
-    console.log('desde action', producto);
+  return (dispatch) => {
+    dispatch(agregarProducto());
+    try {
+      dispatch(agregarProductoExito(producto));
+    } catch (error) {
+      dispatch(agregarProductoError(true));
+    }
   };
 }
+
+const agregarProducto = () => ({
+  type: AGREGAR_PRODUCTO,
+  payload: true,
+});
+
+const agregarProductoExito = (producto) => ({
+  type: AGREGAR_PRODUCTO_EXITO,
+  payload: producto,
+});
+
+const agregarProductoError = () => ({
+  type: AGREGAR_PRODUCTO_ERROR,
+});
