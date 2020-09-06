3523a1b8ad20d3659114ec1d0e2e783e3361ded2
johan Bautista <jsbparra@gmail.com>
2020-09-06 17:36:04 +0200

    crear reducr para las alertas

---
+++ src/reducers/alertaReducer.js
@@ -0,0 +1,19 @@
+import { MOSTRAR_ALERTA, OCULTAR_ALERTA } from '../types';
+
+//cada reducer tiene su propio state
+const initialState = {
+  alerta: null,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case MOSTRAR_ALERTA:
+      return {};
+
+    case OCULTAR_ALERTA:
+      return {};
+
+    default:
+      return state;
+  }
+}
